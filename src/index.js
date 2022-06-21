4070a0bab301b068ec07896eae8639de1046a561
lznval <lizunova.252@yandex.ru>
2022-06-21 15:04:02 +0300

    clean

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
 import ReactDOM from 'react-dom/client';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>    
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
